2bfb0497103946852ca3d049fda0986051a8a3a1
reza <yadegarireza50@googlemail.com>
2025-03-02 11:14:04 +0330

    codes developed

--- shipments/urls.py
+++ shipments/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from .views import create_shipment, shipment_detail, home
+from .views import *
 
 
 
 urlpatterns = [
     path('', home, name = 'home'),
-    path('create/', create_shipment, name='create_shipment'),
-
-    path('shipment/<int:shipment_id>', shipment_detail, name='shipment_detail'),
+    path('save/', save_shipper_and_shipment, name='save_shipper_and_shipment'),
+    # path('create/', create_shipment, name='create_shipment'),
+    #
+    # path('shipment/<int:shipment_id>', shipment_detail, name='shipment_detail'),
 
 ]
 
--- shipments/views.py
+++ shipments/views.py
@@ -1,32 +1,71 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from .forms import ShipperForm, ShipmentForm
-from .models import Shipment
+
 
 def home(request):
-    return render(request, 'home.html')
-def create_shipment(request):
-    if request.method == "POST":
-        shipper_form = ShipperForm(request.POST)
-        shipment_form = ShipmentForm(request.POST)
+    reference_number = request.GET.get('reference_number')
+    shipment = None
+    if reference_number:
+        shipment = get_object_or_404(Shipment, reference_number=reference_number)
+    return render(request, 'home.html', {'shipment': shipment})
 
-        if shipper_form.is_valid() and shipment_form.is_valid():
-            shipper = shipper_form.save()
-            shipment = shipment_form.save(commit=False)
-            shipment.shipper = shipper
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from .models import Shipper, Shipment
+from .forms import ShipperForm, ShipmentForm
+
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Shipper, Shipment
+
+def save_shipper_and_shipment(request):
+    if request.method == 'POST':
+        try:
+            # Save Shipper
+            shipper = Shipper(
+                shipper_name=request.POST['shipper_name'],
+                address=request.POST['address'],
+                country=request.POST['country'],
+                city=request.POST['city'],
+                location=request.POST.get('location', ''),
+                contact_person=request.POST['contact_person'],
+                contact_number=request.POST['contact_number'],
+                mobile_number=request.POST.get('mobile_number', '')
+            )
+            shipper.save()
+
+            # Save Shipment
+            shipment = Shipment(
+                shipper=shipper,
+                receiver_name=request.POST['receiver_name'],
+                receiver_address=request.POST['receiver_address'],
+                receiver_country=request.POST['receiver_country'],
+                receiver_city=request.POST['receiver_city'],
+                receiver_location=request.POST.get('receiver_location', ''),
+                receiver_contact_person=request.POST['receiver_contact_person'],
+                receiver_contact_number=request.POST['receiver_contact_number'],
+                receiver_mobile_number=request.POST.get('receiver_mobile_number', ''),
+                awb_number=request.POST['awb_number'],
+                reference_number=request.POST.get('reference_number', ''),
+                booking_date=request.POST['booking_date'],
+                booking_time=request.POST['booking_time'],
+                product_type=request.POST['product_type'],
+                pieces=request.POST['pieces'],
+                weight=request.POST['weight'],
+                v_weight=request.POST['v_weight'],
+                c_weight=request.POST['c_weight'],
+                item_description=request.POST['item_description'],
+                special_instruction=request.POST.get('special_instruction', ''),
+                cod_amount=request.POST['cod_amount'],
+                base_price=request.POST['base_price'],
+                additional_charges=request.POST['additional_charges']
+            )
             shipment.save()
 
-            return redirect('shipment_detail', shipment_id=shipment.id)
+            # Add a success message
+            messages.success(request, 'Shipper and Shipment saved successfully!')
+        except Exception as e:
+            # Add an error message if something goes wrong
+            messages.error(request, f'An error occurred: {str(e)}')
 
-    else:
-        shipper_form = ShipperForm()
-        shipment_form = ShipmentForm()
+    return render(request, 'shipment_form.html')
 
-    return render(request, 'create_shipment.html', {
-        'shipper_form': shipper_form,
-        'shipment_form': shipment_form,
-    })
-
-
-def shipment_detail(request, shipment_id):
-    shipment = get_object_or_404(Shipment, id=shipment_id)
-    return render(request, 'shipment_detail.html', {'shipment': shipment})
